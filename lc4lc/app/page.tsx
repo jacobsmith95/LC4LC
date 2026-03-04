51b284211a830062c88148d473df654d56c8dfc4
Jacob O. Smith <jacobsmith95@gmail.com>
2026-03-04 10:14:36 -0800

    added grid for biography

--- lc4lc/app/page.tsx
+++ lc4lc/app/page.tsx
@@ -1,10 +1,10 @@
-
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
       <main className="flex min-h-screen w-full max-w-6xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
-        <header className="max-w-5xl text-3xl font-bold leading-10 tracking-tight text-black">
+        <header className="max-w-5xl text-3xl text-center font-bold leading-10 tracking-tight text-black">
           Get 'er Done With Joy Equus Life Coaching
         </header>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
@@ -20,15 +20,31 @@ export default function Home() {
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          <h3 className="max-w-3xl text-4xl font-semibold leading-10 tracking-tight text-black">
-            Meet Your Coach
-          </h3>
-          <p className="max-w-2xl text-lg leading-8 text-zinc-600">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+          <div className="grid grid-cols-2 gap-4 justify-items-center">
+            <div></div>
+            
+            <div>
+              <h3 className="max-w-3xl text-4xl font-semibold leading-10 tracking-tight text-black">
+              Meet Your Coach
+              </h3>
+            </div>
+            <div className="justify-items-center">
+              <Image className=""
+                src="/lori_cook_life_coach.png"
+                width={400}
+                height={400}
+                alt="Picture of Lori Cook"
+              />
+            </div>
+            <div>
+              <p className="max-w-2xl text-lg leading-8 text-zinc-600">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          </div>
         </div>
       </main>
     </div>
