dfc5db371ac2cec7e8f9019ef6be982f494e2744
Jacob O. Smith <jacobsmith95@gmail.com>
2026-03-04 09:00:15 -0800

    removed images and icons

--- lc4lc/app/page.tsx
+++ lc4lc/app/page.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
+      <main className="flex min-h-screen w-full max-w-6xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black">
-            To get started, edit the page.tsx file.
+            Lori Cook - Life Coach
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600">
             Looking for a starting point or more instructions? Head over to{" "}
