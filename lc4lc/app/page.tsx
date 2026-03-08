166b85b83eec5f1cbb9be6b81b98f73ffcb62ac6
Jacob O. Smith <jacobsmith95@gmail.com>
2026-03-07 17:50:51 -0800

    added rounded edge to photos

--- lc4lc/app/page.tsx
+++ lc4lc/app/page.tsx
@@ -11,14 +11,14 @@ export default function Home() {
         <br />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="mt-15 max-w-6xl text-5xl font-semibold leading-10 tracking-tight text-black">
-            <Image className=""
+            <Image className="rounded-xl"
               src="/IMG_20260304_093638.jpg"
               width={1500}
               height={1500}
               alt="Woman connecting with her equine co-coach."
             />
           </h1>
-          <h2 className="max-w-5xl text-5xl font-semibold leading-10 tracking-tight text-black">
+          <h2 className="mt-5 max-w-5xl text-5xl font-semibold leading-10 tracking-tight text-black">
             Discover Your True JOYFUL Life
           </h2>
           <p className="max-w-2xl text-lg leading-8 text-zinc-600">
@@ -29,14 +29,13 @@ export default function Home() {
           </p>
           <p></p>
           <div className="mt-10 grid grid-cols-2 gap-4 justify-items-center">
-            
             <div className="col-span-2">
               <h3 className="max-w-3xl text-4xl font-semibold leading-10 tracking-tight text-black">
               Meet Your Coach
               </h3>
             </div>
             <div className="justify-items-center">
-              <Image className=""
+              <Image className="rounded-lg"
                 src="/lori_cook_life_coach.png"
                 width={400}
                 height={400}
