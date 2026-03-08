79c73e38f383ed179ac2cd393b133df3e776c219
Jacob O. Smith <jacobsmith95@gmail.com>
2026-03-07 19:47:23 -0800

    added placeholder testimonial text for layout design

--- lc4lc/app/page.tsx
+++ lc4lc/app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
-      <main className="flex min-h-screen w-full max-w-6xl flex-col items-center justify-between px-15 bg-white sm:items-start">
+    <div className="flex min-h-screen justify-center bg-zinc-50 font-sans">
+      <main className="flex min-h-screen w-full max-w-6xl flex-col px-15 bg-white sm:items-start">
         <nav className="mt-10 max-w-5xl text-5xl text-center font-bold tracking-tight text-black">
           Get 'er Done With Joy Equus Life Coaching
           <br />
@@ -27,14 +27,38 @@ export default function Home() {
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          <h3 className="mt-5 max-w-5xl text-4xl text-center font-semibold leading-10 tracking-tight text-black text-shadow-lg">
+          <p className="mt-5 max-w-5xl text-4xl text-center font-semibold tracking-tight text-black text-shadow-lg">
             Lives Changed
-          </h3>
-          <div className="mt-5 grid grid-cols-2 gap-4 justify-items-center mb-10">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+          </p>
+          <div className="mt-5 grid grid-cols-2 gap-4 justify-items-center text-center mb-5">
+            <div className="text-lg leading-8 text-zinc-600">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                <br/><br/>
+                Lorem ipsum
+              </p>
+            </div>
+            <div className="text-lg leading-8 text-zinc-600">
+              <p>
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                <br/><br/>
+                Lorem ipsum
+              </p>
+            </div>
+            <div className="mt-5 text-lg leading-8 text-zinc-600">
+              <p>
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                <br/><br/>
+                Lorem ipsum
+              </p>
+            </div>
+            <div className="mt-5 text-lg leading-8 text-zinc-600">
+              <p>
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <br/><br/>
+                Lorem ipsum
+              </p>
+            </div>
           </div>
           <p></p>
           <div className="mt-10 grid grid-cols-2 gap-4 justify-items-center mb-15">
