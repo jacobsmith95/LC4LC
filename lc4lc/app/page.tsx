b8ba3cbdfe09b5bddbca560408b258561eb4def7
Jacob O. Smith <jacobsmith95@gmail.com>
2026-03-07 17:26:44 -0800

    added header image

--- lc4lc/app/page.tsx
+++ lc4lc/app/page.tsx
@@ -2,14 +2,21 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
       <main className="flex min-h-screen w-full max-w-6xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
-        <header className="max-w-5xl text-3xl text-center font-bold leading-10 tracking-tight text-black">
+        <header className="max-w-5xl text-5xl text-center font-bold tracking-tight text-black">
           Get 'er Done With Joy Equus Life Coaching
+          <br />
         </header>
+        <br />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-5xl text-5xl font-semibold leading-10 tracking-tight text-black">
-            
+          <h1 className="mt-10 max-w-6xl text-5xl font-semibold leading-10 tracking-tight text-black">
+            <Image className=""
+              src="/IMG_20260304_093638.jpg"
+              width={1500}
+              height={1500}
+              alt="Woman connecting with her equine co-coach."
+            />
           </h1>
           <h2 className="max-w-5xl text-5xl font-semibold leading-10 tracking-tight text-black">
             Discover Your True JOYFUL Life
@@ -20,6 +27,7 @@ export default function Home() {
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
+          <p></p>
           <div className="grid grid-cols-2 gap-4 justify-items-center">
             <div></div>
             
@@ -33,7 +41,7 @@ export default function Home() {
                 src="/lori_cook_life_coach.png"
                 width={400}
                 height={400}
-                alt="Picture of Lori Cook"
+                alt="Picture of Lori Cook."
               />
             </div>
             <div>
