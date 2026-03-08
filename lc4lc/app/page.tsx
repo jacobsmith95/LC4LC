2c8f4841cb639bdccae5c6a5974c05d219e4bc6f
Jacob O. Smith <jacobsmith95@gmail.com>
2026-03-07 17:31:21 -0800

    fixed margins, padding, and centered coach bio title

--- lc4lc/app/page.tsx
+++ lc4lc/app/page.tsx
@@ -3,14 +3,14 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
-      <main className="flex min-h-screen w-full max-w-6xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
+      <main className="flex min-h-screen w-full max-w-6xl flex-col items-center justify-between py-15 px-15 bg-white sm:items-start">
         <header className="max-w-5xl text-5xl text-center font-bold tracking-tight text-black">
           Get 'er Done With Joy Equus Life Coaching
           <br />
         </header>
         <br />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="mt-10 max-w-6xl text-5xl font-semibold leading-10 tracking-tight text-black">
+          <h1 className="mt-15 max-w-6xl text-5xl font-semibold leading-10 tracking-tight text-black">
             <Image className=""
               src="/IMG_20260304_093638.jpg"
               width={1500}
@@ -28,10 +28,9 @@ export default function Home() {
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
           <p></p>
-          <div className="grid grid-cols-2 gap-4 justify-items-center">
-            <div></div>
+          <div className="mt-10 grid grid-cols-2 gap-4 justify-items-center">
             
-            <div>
+            <div className="col-span-2">
               <h3 className="max-w-3xl text-4xl font-semibold leading-10 tracking-tight text-black">
               Meet Your Coach
               </h3>
