049b12a99b54b4acbf00c6f78d2511945291e957
Jacob O. Smith <jacobsmith95@gmail.com>
2026-05-27 09:23:27 -0700

    added navbar to layout and changed color

--- lc4lc/app/layout.tsx
+++ lc4lc/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./navbar.tsx"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`font-(family-name:--font-sans) max-w-full`}>
+        <div className="flex flex-col overflow-x-clip">
+          <Navbar />
+          <main className="flex flex-grow flex-col">{children}</main>
+        </div>
       </body>
     </html>
   );
