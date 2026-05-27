bb664f0ebc8c2f1cdd76bb2c44bb6bf0c1e16ecc
Jacob O. Smith <jacobsmith95@gmail.com>
2026-05-27 11:25:02 -0700

    rounded and centered navbar on top of page

--- lc4lc/app/navbar.tsx
+++ lc4lc/app/navbar.tsx
@@ -2,27 +2,27 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 left-0 z-50 w-full bg-lavender text-white md:px-8 py-4">
-      <div className="w-full flex justify-between items-center md:px-8 py-2">
+    <nav className="sticky top-3 left-30 z-50 w-9/10 bg-lavender text-white md:px-8 py-4 rounded-xl">
+      <div className="w-full flex justify-between items-center md:px-8 py-2 text-xl">
         <section className="flex space-x-4">
-          <Link href="/link1" className="hover:underline">
+          <Link href="/link1" className="hover:text-lorigreen">
             Link 1
           </Link>
-          <Link href="/link2" className="hover:underline">
+          <Link href="/link2" className="hover:text-lorigreen">
             Get Er Done With Joy Equus Life Coaching
           </Link>
         </section>
 
         <section className="flex justify-center">
-          <Link href="/" className="text-lg font-semibold hover:underline">
+          <Link href="/" className="">
           </Link>
         </section>
 
         <section className="flex space-x-4">
-          <Link href="/link4" className="hover:underline">
+          <Link href="/link4" className="hover:text-lorigreen">
             About
           </Link>
-          <Link href="/link5" className="hover:underline">
+          <Link href="/link5" className="hover:text-lorigreen">
             Contact
           </Link>
         </section>
